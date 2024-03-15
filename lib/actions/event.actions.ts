77b123e5ecfefd84c9b3236c599e7f55c32fc66f
YashChouhan <yashchouhan.it@gmail.com>
2024-03-15 17:22:30 +0530

    successfull created events logic and and they are also showing in the event page after compeletion of event

--- lib/actions/event.actions.ts
+++ lib/actions/event.actions.ts
@@ -33,9 +33,14 @@ export async function createEvent({ userId, event, path }: CreateEventParams) {
     await connectToDatabase()
 
     const organizer = await User.findById(userId)
+    console.log("ORGANIZER_ID --> ", organizer)
     if (!organizer) throw new Error('Organizer not found')
 
-    const newEvent = await Event.create({ ...event, category: event.categoryId, organizer: userId })
+    const newEvent = await Event.create({
+      ...event,
+      category: event.categoryId,
+      organizer: userId
+    })
     revalidatePath(path)
 
     return JSON.parse(JSON.stringify(newEvent))
